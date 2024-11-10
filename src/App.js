7b2db9a4c1c759430a9e5e52ee67765ed9c915f7
AshBatman <ashrit221@gmail.com>
2024-11-10 18:05:58 +0530

    fix: rename folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StartPage from "./components/StartPage";
-import DocsPage from "./components/Docs";
-import DocumentCollab from "./components/DocumentCollab";
+import StartPage from "./pages/StartPage";
+import DocsPage from "./pages/Docs";
+import DocumentCollab from "./pages/DocumentCollab";
 
 const App = () => {
   return (
